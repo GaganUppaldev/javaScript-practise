39352fdfe5507f8940cb2a9f5714b774260886d8
GaganUppaldev <149155501+GaganUppaldev@users.noreply.github.com>
2024-07-31 12:50:37 +0530

    Update Arrays.js
    JavaScript Array revision from official documentation is over Revised methods to access every item from array , use map , filter , split , join and to string (to convert an array to string)
    
    Link of official documentation of array part is here:
    https://developer.mozilla.org/en-US/docs/Learn/JavaScript/First_steps/Arrays#what_is_an_array

--- Arrays.js
+++ Arrays.js
@@ -58,3 +58,64 @@ if (missionvalue !== -1) {
 //number of element we want to remove, if we convert it 2 than it will remove one more next element means total remove will be of two elements ,eg if we add lol in mission array and replace 1 with 2 than gamma and lol both removed
 }
 console.log(mission)
+
+//Acessing every item from array
+
+//using for of  statement
+const acess = ["john" , "jane" , "james" , "jimmy" , "joseph"]
+console.log(acess);
+
+for (const aces of acess){
+  console.log(aces);
+}// we got each item seprately
+
+//using map
+
+//cretaed function named multiply and gave acess to each item inside array named triple
+function multiply(tripl){   
+  return tripl * 3;
+}
+const triple = [1,2,3,4,5]
+const final = triple.map(multiply);//used map to multiply each item inside array
+console.log(final); 
+
+//filter
+//used to create a new array with all items that pass the test
+
+function Verify(countrie){
+  return countrie.length > 5;
+}
+
+const countries = ["usa" , "uk" , "india" , "australia" , "canada" ]
+
+const verified = countries.filter(Verify);
+console.log(verified)
+
+//conversion between strings and arrays
+const planets = ["mercury","venus","earth","mars","jupiter","saturn","uranus","neptune"];
+console.log(planets)// all planents 
+//Now see length of array plants
+console.log(planets.length);//lenth is 8
+console.log(planets[0])//mercury is output
+
+//But problem rises here , This is how react framerwork takes input from user by they default it covert input to string for security reasons so important to learn from this case
+
+const level =  "high,mid,low"//Now this is string 
+console.log(level)
+console.log(level[0]) //h will be the out put
+//Now lets solve this By use of split method
+const split = level.split(",");//split where (, ) is present)
+console.log(split)
+console.log(split[0])//Now it will return high
+console.log(split.length) // length is 3
+console.log(split.length-1) //It will return index of last item inside array here is 2 , as 0,1,2
+
+//Join method is exact similar to split method
+
+//tostring method : used to convert array to string
+const Birds = ["parrot" ,"peacock","owl","sparrow"]
+console.log(Birds)
+console.log(Birds[0])
+
+const end = Birds.toString()
+console.log(end[0])
