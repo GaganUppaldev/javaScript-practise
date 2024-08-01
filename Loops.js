fc56cf3308edc371481ce9b7c7d5206405f190dd
GaganUppaldev <149155501+GaganUppaldev@users.noreply.github.com>
2024-08-01 09:42:10 +0530

    Update Loops.js
    Added standard loop concepts here

--- Loops.js
+++ Loops.js
@@ -33,3 +33,38 @@ const extract2 = fishs.filter((fishs) => fishs.startsWith("g"))//here i suppored
 console.log(extract2)
 
 //Note js is case sensitive and startsWith "W" is capital
+
+//2> standard loop 
+
+//used when we to run same code multiple times
+/* BASIC SUNTAX BELOW
+for (initializer; condition; final-expression) {
+  // code to run
+}*/
+function cal(){
+  //const empty = []; //initialized an empty array
+  for(let i = 1 ; i <=10 ; i++) {
+    const sol = `2 * ${i} = ${2*i}`
+    console.log(sol)
+    
+    
+  }
+  //return empty // returned an empty array
+}
+
+//const work = cal()
+//console.log(work) // Returning table of 2 very perfect but was showing undefined at end beacuse function cal was not returning anything so lets solve that , Two ways first only just type cal() and remove work but i will return an empty array to solve this problem 
+//But first approch is best so i am now applying that it will return [] at end
+
+cal()//worked perfectly 
+
+//Doing for-of example with standard loop
+
+const elements = ["Nitrogen" , "sodium" ,"oxygen"]
+// we will use elements.lenth
+for (let i = 0 ; i < elements.length ; i++){
+
+  console.log(elements[i])
+  
+}
+//above undifened problem was caused due to i <= elements.length because there is nothing at index 3 so it will show undefined so to fix it only < to get items till 0,1,2
