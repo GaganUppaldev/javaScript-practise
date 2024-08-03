cba6c93266eba6c2702c08959a430fca93706133
GaganUppaldev <149155501+GaganUppaldev@users.noreply.github.com>
2024-08-03 10:28:04 +0530

    Create DOM with evenListeners.js
    This is .js file ocver topic of DOM and eventListeners , i created two buttons one change color and other will tell you clicked , i used my own approch to made them 
    This file is connected to  DOM with evenListeners.html

---
+++ DOM with evenListeners.js
@@ -0,0 +1,27 @@
+const x = document.getElementById("btn");
+const para = document.getElementById("para");
+const knowbutton=document.getElementById("click");
+
+//btn 
+x.addEventListener("click", () => {
+    document.body.style.backgroundColor = "pink";
+    const value = value+1;
+
+    
+
+});
+
+
+
+
+//click
+knowbutton.addEventListener("click",() => {
+
+    
+    para.innerHTML++;
+
+    
+
+   
+
+});
